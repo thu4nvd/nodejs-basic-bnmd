799b0e7a7b4938175983bf0f467badfceb8eaf67
Thuan VU <thuanvd@outlook.com>
2023-07-01 21:21:36 +0700

    Vid 6: view engine setup

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine.js';
+
+const app = express();
+const port = 8080
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('test/index.ejs')
+})
+app.get('/about', (req, res) => {
+    res.send(`I'm Eric!`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
