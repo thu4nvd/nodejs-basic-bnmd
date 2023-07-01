ee56f9979b3951589c71e38abe8a6d9a73f723d1
Thuan VU <thuanvd@outlook.com>
2023-07-01 17:11:15 +0700

    hello world with expressjs framework

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from THUANVD</h2>');
-    res.end();
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World by THUANVD!</h1>')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/about', (req, res) => {
+    res.send(`I 'm THuanvd`)
 })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
